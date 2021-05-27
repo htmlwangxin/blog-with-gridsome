f9b5672b4eed3383ace033181a5fd5bec9a3e683
allen <allen.wang@yunlsp.com>
2021-05-27 14:52:32 +0800

    1

---
+++ src/pages/Index.vue
@@ -0,0 +1,110 @@
+<template>
+  <Layout>
+
+    <!-- Page Header-->
+    <header class="masthead"
+      :style="{
+          background:`url(${GRIDSOME_API_URL+general.cover.url})`
+      }">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{general.title}}</h1>
+              <span class="subheading">{{general.subtitle}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- Main Content-->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview"
+            v-for="edge in $page.posts.edges"
+            :key="edge.node.id">
+            <g-link :to="'/post/'+edge.node.id">
+              <h2 class="post-title">{{edge.node.title}}</h2>
+              <!-- <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3> -->
+            </g-link>
+            <p class="post-meta">
+              Posted by
+              <a href="#!">Start Bootstrap</a>
+              on {{edge.node.created_at}}
+            </p>
+            <span v-for="tag in edge.node.tags"
+              :key="tag.id">
+              <g-link :to="'/tag/'+tag.id">{{tag.title}}</g-link> &nbsp;
+            </span>
+            <hr />
+          </div>
+          <!-- Pager-->
+          <Pager :info="$page.posts.pageInfo" />
+          <!-- <div class="clearfix"><a class="btn btn-primary float-right"
+              href="#!">Older Posts →</a></div> -->
+        </div>
+      </div>
+    </div>
+  </Layout>
+
+</template>
+<page-query>
+query($page: Int){
+  posts:allStrapiPost(perPage:2,page:$page)@paginate{
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges{
+      node{
+        id
+        title
+        content
+        tags{
+          id
+          title
+        }
+        created_at
+      }
+    }
+  }
+  general:allStrapiGeneal{
+    edges{
+      node{
+        id
+        title
+        subtitle
+        cover{
+          url
+        }
+      }
+    }
+  }
+}
+</page-query>
+<script>
+import { Pager } from 'gridsome'
+export default {
+  name: 'HomePage',
+  metaInfo: {
+    title: 'Hello, world!'
+  },
+  components: {
+    Pager
+  },
+  computed: {
+    general() {
+      return this.$page.general.edges[0].node
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style>
+</style>
