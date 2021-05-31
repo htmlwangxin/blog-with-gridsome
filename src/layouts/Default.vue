46fb4ba9617a0ba06f90cff31429cfc4dea1afe4
allen <allen.wang@yunlsp.com>
2021-05-31 09:30:26 +0800

    删除入口

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,8 +3,6 @@
     <nav class="navbar navbar-expand-lg navbar-light fixed-top"
       id="mainNav">
       <div class="container">
-        <a class="navbar-brand"
-          href="index.html">Start Bootstrap</a>
         <button class="navbar-toggler navbar-toggler-right"
           type="button"
           data-toggle="collapse"
@@ -18,12 +16,6 @@
         <div class="collapse navbar-collapse"
           id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link"
-                href="index.html">Home</a></li>
-            <li class="nav-item"><a class="nav-link"
-                href="about.html">About</a></li>
-            <li class="nav-item"><a class="nav-link"
-                href="post.html">Sample Post</a></li>
             <li class="nav-item">
               <g-link class="nav-link"
                 to="/contact">Contact</g-link>
